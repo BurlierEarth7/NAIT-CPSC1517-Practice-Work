3b97009e930b43f8f17662483ea85a1357039f1b
BurlierEarth7 <j.tollefson@outlook.com>
2025-01-23 12:35:40 -0700

    Create LinkingTests
    Added images, instructions for linking, and empty test and project to fill in

---
+++ Linking Tests/LinkingTests/LinkMe.cs
@@ -0,0 +1,5 @@
+﻿namespace LinkingTests;
+
+public class LinkMe
+{
+}
---
+++ Linking Tests/UnitTestsLinkingTests/LinkMe_Should.cs
@@ -0,0 +1,10 @@
+namespace UnitTestsLinkingTests;
+
+public class LinkMe_Should
+{
+    [Fact]
+    public void Tests_Are_Linked()
+    {
+        
+    }
+}
