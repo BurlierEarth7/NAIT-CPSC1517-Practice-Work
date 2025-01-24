6e233f01a0f34d6648fe6488f502253e0e5e68fb
BurlierEarth7 <j.tollefson@outlook.com>
2025-01-24 15:21:25 -0700

    Add Unit Tests
    added Assembly_Reference_Exists(): checks if an assembly reference to LinkingTests has been created

--- Linking Tests/UnitTestsLinkingTests/LinkMe_Should.cs
+++ Linking Tests/UnitTestsLinkingTests/LinkMe_Should.cs
@@ -1,10 +1,20 @@
+using FluentAssertions;
+
 namespace UnitTestsLinkingTests;
 
 public class LinkMe_Should
 {
+    /// <summary>
+    /// Checks if an Assembly Reference to LinkingTests has been created.
+    /// </summary>
     [Fact]
-    public void Tests_Are_Linked()
+    public void Assembly_Reference_Exists()
     {
-        
+        // Given
+        string @asmName = @"LinkingTests.LinkMe, LinkingTests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+        // When
+        Type? classType = Type.GetType(@asmName);
+        // Then
+        classType.Should().NotBeNull();
     }
 }
