e222f017f8ecf5e9e6d08f441bfbbb50a1eda44d
BurlierEarth7 <j.tollefson@outlook.com>
2025-01-24 15:32:38 -0700

    Update test name + add credits & comments
    Updated the test name to be more accurate
    
    Added a comment including a link to references for this solution
    
    Added comments to explain what the test is performing

--- Linking Tests/UnitTestsLinkingTests/LinkMe_Should.cs
+++ Linking Tests/UnitTestsLinkingTests/LinkMe_Should.cs
@@ -5,16 +5,23 @@ namespace UnitTestsLinkingTests;
 public class LinkMe_Should
 {
     /// <summary>
-    /// Checks if an Assembly Reference to LinkingTests has been created.
+    /// Checks if LinkingTests has been linked properly.
     /// </summary>
     [Fact]
-    public void Assembly_Reference_Exists()
+    public void Test_IsLinked()
     {
         // Given
-        string @asmName = @"LinkingTests.LinkMe, LinkingTests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+        //* Credits to moribvndvs on https://stackoverflow.com/a/8499606 for this solution!
+        string asmName = @"LinkingTests.LinkMe, LinkingTests, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null";
+
         // When
-        Type? classType = Type.GetType(@asmName);
+        //? Reflects the type. (Reflection describes Types that can describe assemblies)
+        //? Will only return the Type if the current assembly has a reference to the LinkingTests project
+        Type? classType = Type.GetType(asmName);
+
+
         // Then
-        classType.Should().NotBeNull();
+        //? Checks if the Type is not null (i.e. cannot be created, due to the lack of Project Reference)
+        classType.Should().NotBeNull("a project reference to LinkingTests is expected");
     }
 }
