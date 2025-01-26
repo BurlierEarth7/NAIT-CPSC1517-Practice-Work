bfd15d3ac79beefb8b7b5e51455383f73733a6a8
BurlierEarth7 <j.tollefson@outlook.com>
2025-01-25 22:57:04 -0700

    Begin Unit Tests for FlagEnums

--- Intro to Enums/Enumerables/FlagEnums.cs
+++ Intro to Enums/Enumerables/FlagEnums.cs
@@ -1,3 +1,14 @@
 namespace Enumerables;
 
-// Create a public 
+// Create a public FlagAttribute Enum called FileOperations
+// Add the values "None", "ReadOnly", "Hidden", "Compressed", and "Encrypted"
+// Add the Base 2 Values, where None = 0, ReadOnly = 1, etc...
+
+[Flags]
+public enum FileOperations {
+    None = 0,
+    ReadOnly = 1,
+    Hidden = 2,
+    Compressed = 4,
+    Encrypted = 8
+}
